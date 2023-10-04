082f529731891235a062c568c239212c536a2817
KhalifahMB <muhammadelkufahn27@gmail.com>
2023-10-04 10:46:03 +0100

    second commit

--- script.js
+++ script.js
@@ -1,139 +1,104 @@
+// Query DOM elements
 const links = document.querySelectorAll(".link");
 const steps = document.querySelectorAll(".step");
-
-//function for checking form validity
 const form = document.getElementById("form");
-function checkFormValidity() {
-  if (!form.reportValidity()) {
-    const inValidInputs = form.querySelectorAll(":invalid");
-    inValidInputs.forEach((input) => input.classList.add("error"));
-    return false;
-  }
-  return true;
-}
+const btns = document.querySelectorAll(".btn");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const plans = document.querySelectorAll(".plan");
+const bonus = document.querySelectorAll(".bonus");
 
-// handling changes form inputs
+// User data object to store form input values
 let userData = {
   name: "",
   email: "",
   number: "",
   plan: "mo",
 };
-const handleStore = (input) => {
-  const { name, value } = input;
-  userData[name] = value;
-};
 
-// navigating to next or current step
+// Current step tracking
 let currentStep = 1;
 
-const btns = document.querySelectorAll(".btn");
-const prevBtn = document.getElementById("prevBtn");
-const nextBtn = document.getElementById("nextBtn");
-const changeActiveLink = () => {
+// Function to update active link and button visibility
+function updateUI() {
   links.forEach((link) => {
-    // console.log(link.dataset.id);
     link.classList.remove("active");
     if (link.dataset.id == `step-${currentStep}`) {
       link.classList.add("active");
     }
   });
-  if (currentStep == 1) {
+
+  if (currentStep === 1) {
     prevBtn.style.display = "none";
-  } else if (currentStep == 5) {
+  } else if (currentStep === 5) {
     btns.forEach((btn) => (btn.style.display = "none"));
   } else {
     btns.forEach((btn) => (btn.style.display = "block"));
   }
-};
-function nextStep() {
-  const formIsValid = checkFormValidity();
-  switch (currentStep) {
-    case 1:
-      if (formIsValid) {
-        currentStep += 1;
-        break;
-      }
-      currentStep = 1;
-      break;
-    case 2:
-      currentStep += 1;
-      break;
-    case 3:
-      nextBtn.innerText = "Confirm";
-      nextBtn.classList.add("confirm");
-      currentStep += 1;
-      break;
-    case 4:
-      currentStep += 1;
-      break;
-    case 5:
-      console.log(5);
-      currentStep += 1;
-      btns.forEach((btn) => (btn.style.display = "none"));
-      prevBtn.style.display = "none";
-      break;
-
-    default:
-      break;
-  }
-  // if (currentStep == 1) {
-  //
-  // }
-  // if (currentStep < 4) {
-  //   currentStep += 1;
-  // } else {
-  //   currentStep = 3;
-  // }
-  changeActiveLink();
 }
-nextBtn.addEventListener("click", () => {
-  nextStep();
-  steps.forEach((step) => step.classList.add("d-none"));
-  document.getElementById(`step-${currentStep}`).classList.remove("d-none");
-});
-prevBtn.addEventListener("click", () => {
-  if (currentStep < 2) {
-    currentStep = 1;
-  } else {
-    currentStep -= 1;
+
+// Function to check form validity
+function isFormValid() {
+  return form.reportValidity();
+}
+
+// Function to handle form input changes and update userData
+function handleStore(input) {
+  const { name, value } = input;
+  userData[name] = value;
+}
+
+// Function to navigate to the next step
+function nextStep() {
+  if (currentStep === 1 && !isFormValid()) {
+    const inValidInputs = form.querySelectorAll(":invalid");
+    inValidInputs.forEach((input) => input.classList.add("error"));
+
+    return; // Don't proceed if the form is invalid on the first step
   }
-  changeActiveLink();
+
+  if (currentStep === 3) {
+    nextBtn.innerText = "Confirm";
+    nextBtn.classList.add("confirm");
+  }
+
+  currentStep = Math.min(currentStep + 1, 5);
+  updateUI();
   steps.forEach((step) => step.classList.add("d-none"));
   document.getElementById(`step-${currentStep}`).classList.remove("d-none");
-  changeActiveLink();
-});
+}
 
-// funtionality fo  tiggling plan (yearly or monthly)
-const toggler = document.getElementById("toggler");
-const plans = document.querySelectorAll(".plan");
-const bonus = document.querySelectorAll(".bonus");
-const showBonus = () => {
-  bonus.forEach((bonus) => (bonus.style.display = "block"));
-};
-const hideBonus = () => {
-  bonus.forEach((bonus) => (bonus.style.display = "none"));
-};
+// Function to navigate to the previous step
+function prevStep() {
+  currentStep = Math.max(currentStep - 1, 1);
+  updateUI();
+  steps.forEach((step) => step.classList.add("d-none"));
+  document.getElementById(`step-${currentStep}`).classList.remove("d-none");
+}
 
+// Function to change plan (monthly or yearly)
 function changePlan(sub) {
   plans.forEach((plan) => (plan.innerHTML = sub));
 }
+
+// Function to handle plan toggling
 function handleChange(e) {
   if (e.checked) {
-    showBonus();
+    bonus.forEach((bonus) => (bonus.style.display = "block"));
     changePlan("yr");
     userData.plan = "yr";
     toggler.style.transform = "translateX(20px)";
   } else {
-    hideBonus();
+    bonus.forEach((bonus) => (bonus.style.display = "none"));
     changePlan("mo");
     userData.plan = "mo";
     toggler.style.transform = "translateX(0)";
   }
 }
 
-(function () {
-  if (currentStep == 1) {
-    prevBtn.style.display = "none";
-  }
-})();
+// Event listeners
+nextBtn.addEventListener("click", nextStep);
+prevBtn.addEventListener("click", prevStep);
+
+// Initial UI setup
+updateUI();
